e37c1310348b16f5ecece36c7795901afa646eb8
Jeremy <jeremy.rousseau2@outlook.fr>
2024-09-27 11:52:56 +0200

    Update SLURM execution

--- scripts/slurm_toto_search_homologs.sh
+++ scripts/slurm_toto_search_homologs.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-#SBATCH --job-name=TOTOXXXX
-#SBATCH --output=/shared/projects/darkdino/TOTOXXXX_%j.out
+#SBATCH --job-name=totophd
+#SBATCH --output=/shared/projects/darkdino/test_ue_lucie/toto_a_banyuls/scripts/totophd_%j.out
 
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 
 #SBATCH --partition=fast
 
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=4GB
 
 module load conda
 
-source activate /shared/projects/darkdino/conda/envs
+cd /shared/projects/darkdino/test_ue_lucie/toto_a_banyuls/scripts
 
-cd /shared/projects/darkdino
+SCRIPT="/shared/projects/darkdino/test_ue_lucie/toto_a_banyuls"
+FASTA="/shared/projects/darkdino/test_ue_lucie/toto_a_banyuls/test/sequences.fasta"
+TARA="/shared/projects/darkdino/test_ue_lucie/toto_a_banyuls/database/OMID_supegal40sp_metaT.aa"
+WORKDIR="/shared/projects/darkdino/test_ue_lucie/toto_a_banyuls/analysis"
+CONDAENV="/shared/projects/darkdino/conda/envs/toto_a_banyuls"
 
-run=8
-fasta="test/list_sequence.fasta"
-cpus=8
-
-./toto_search_homologs.sh -r $run -f $fasta -c 8
+bash $SCRIPT/main.sh -f $FASTA -t $TARA -w $WORKDIR -e $CONDAENV -r 8 -c 10
