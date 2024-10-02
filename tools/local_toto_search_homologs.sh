ef32688ade9c4e1fa864264c5bd983b7e4385f3f
Jeremy <jeremy.rousseau2@outlook.fr>
2024-10-02 17:09:20 +0200

    update test

--- tools/local_toto_search_homologs.sh
+++ tools/local_toto_search_homologs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 SCRIPT="/home/jrousseau/Documents/git_projects/toto_a_banyuls/tools/toto_search_homologs"
-FASTA="/home/jrousseau/Documents/git_projects/toto_a_banyuls/test/sequences.fasta"
-TARA="/home/jrousseau/Documents/git_projects/toto_a_banyuls/test/OMID_supegal40sp_metaT.100000.aa"
-WORKDIR="/home/jrousseau/Documents/git_projects/toto_a_banyuls/analysis/TEST"
-GENE="TEST"
+FASTA="/home/jrousseau/Documents/git_projects/toto_a_banyuls/analysis/sequences.fasta"
+TARA="/home/jrousseau/Documents/git_projects/toto_a_banyuls/database/OMID_supegal40sp_metaT.100000.aa"
+WORKDIR="/home/jrousseau/Documents/git_projects/toto_a_banyuls/analysis/SEQUENCES"
+GENE="SEQUENCES"
 CONDAENV="/home/jrousseau/miniforge3/envs/toto_a_banyuls"
 
 bash $SCRIPT/main.sh -f $FASTA -t $TARA -w $WORKDIR -g $GENE -e $CONDAENV -r 8 -c 10bash $SCRIPT/main.sh -f $FASTA -t $TARA -w $WORKDIR -e $CONDAENV -r 8 -c 10
