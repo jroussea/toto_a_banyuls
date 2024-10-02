6b87b11fa699ba5940f9577366ead744841f6acb
Jeremy <jeremy.rousseau2@outlook.fr>
2024-10-02 20:14:30 +0200

    update conda environment

--- tools/local_toto_search_homologs.sh
+++ tools/local_toto_search_homologs.sh
@@ -4,7 +4,6 @@ SCRIPT="/home/jrousseau/Documents/git_projects/toto_a_banyuls/tools/toto_search_
 FASTA="/home/jrousseau/Documents/git_projects/toto_a_banyuls/analysis/sequences.fasta"
 TARA="/home/jrousseau/Documents/git_projects/toto_a_banyuls/database/OMID_supegal40sp_metaT.100000.aa"
 WORKDIR="/home/jrousseau/Documents/git_projects/toto_a_banyuls/analysis/SEQUENCES"
-GENE="SEQUENCES"
-CONDAENV="/home/jrousseau/miniforge3/envs/toto_a_banyuls"
+GENE="SEQUENCES-2"
 
-bash $SCRIPT/main.sh -f $FASTA -t $TARA -w $WORKDIR -g $GENE -e $CONDAENV -r 8 -c 10
+bash $SCRIPT/main.sh -f $FASTA -t $TARA -w $WORKDIR -g $GENE -r 8 -c 10
